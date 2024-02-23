2c8a2787844c57df6ebd87de19eba5c0b0f6c2ad
GabrielVasconcel <ghv@cin.ufpe.br>
2024-02-23 18:28:52 -0300

    Update separate_vocals_instruments.py
    Mudança para permitir uma ou duas músicas como entrada

--- separate_vocals_instruments.py
+++ separate_vocals_instruments.py
@@ -1,11 +1,12 @@
 import spleeter
 from spleeter.separator import Separator
 
-def separate_vocals_instruments(input_music1, input_music2, output_music1, output_music2): # Definir a função que vai separar os 2 inputs
+def separate_vocals_instruments(input_music1, output_music1, input_music2=None, output_music2=None): # Definir a função que vai separar os 2 inputs
     separator = Separator('spleeter:2stems')
 
     # Separação dos vocais e melodia da música 1
     separator.separate_to_file(input_music1, output_music1)
 
     # Separação dos vocais e melodia da música 2
-    separator.separate_to_file(input_music2, output_music2)
+    if input_music2 and output_music2:
+        separator.separate_to_file(input_music2, output_music2)
