bbef764cebfc28eeca742258154468518dc0cd4c
GabrielVasconcel <ghv@cin.ufpe.br>
2024-02-23 15:52:14 -0300

    Create separate_vocals_instruments.py

---
+++ separate_vocals_instruments.py
@@ -0,0 +1,11 @@
+import spleeter
+from spleeter.separator import Separator
+
+def separate_vocals_instruments(input_music1, input_music2, output_music1, output_music2): # Definir a função que vai separar os 2 inputs
+    separator = Separator('spleeter:2stems')
+
+    # Separação dos vocais e melodia da música 1
+    separator.separate_to_file(input_music1, output_music1)
+
+    # Separação dos vocais e melodia da música 2
+    separator.separate_to_file(input_music2, output_music2)
